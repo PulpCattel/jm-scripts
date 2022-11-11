0eb36ea6d07a17c26df272434e19daad4133924d
PulpCattel <PulpCattel@users.noreply.github.com>
2022-11-11 13:18:43 +0000

    Add jmsim.py

--- README.md
+++ README.md
@@ -8,9 +8,10 @@ Usage examples are below.
 
 ### Scripts
 
-* [`jmfinder.py`](jmfinder.py)
+* [`jmfinder.py`](#jmfinder)
+* [`jmsim.py`](#jmsim)
 
-## jmfinder.py
+## jmfinder
 ```sh
 python3 jmfinder.py -h
 usage: jmfinder.py [options] start [end]
@@ -47,7 +48,7 @@ The only requirement is a Bitcoin Core node running with [REST interface](https:
 Pass `-rest` through CLI or set `rest=1` in `bitcoin.conf`
 
 ### Example
-```shell
+```commandline
 python3 jmfinder.py 762603
 11/10/2022 07:48:20 INFO: Scanning from block 762603 to block 762621
 11/10/2022 07:48:20 INFO: 
@@ -94,6 +95,95 @@ Locktime: 3479639216
 11/10/2022 07:48:21 INFO: Processed block 762620.
 11/10/2022 07:48:22 INFO: Processed block 762621.
 11/10/2022 07:48:22 INFO: Scan completed
+```
+
+---
+
+## jmsim
+```commandline
+python3 jmsim.py -h
+usage: jmsim.py [options] orderbook
+
+Given an orderbook file as JSON, e.g., exported from ob-watcher.py, run multiple simulations to estimate the picking chances of each fidelity bond.
+
+positional arguments:
+  orderbook             Path to the exported orderbook in JSON format
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -n MAKER_COUNT, --maker-count MAKER_COUNT
+                        Number of counterparties to use in CoinJoin simulation, default 10
+  -t TRIALS, --trials TRIALS
+                        Number of trials to estimate picking chances, default 10
+  -s SAMPLE_SIZE, --sample_size SAMPLE_SIZE
+                        Number of CoinJoin simulations for each trial, default 100 times the number of (filtered) offers in the orderbook
+  -b BONDLESS_ALLOWANCE, --bondless BONDLESS_ALLOWANCE
+                        Bondless allowance to use for CoinJoin simulation (instead of bondless_makers_allowance default, 0.125)
+  -a AMOUNT, --amount AMOUNT
+                        Filter orderbook based on CoinJoin amount in sats. If an offer can't satisfy this amount, it's disregarded. Default does not filter
+  -f MAX_FEES, --max-fees MAX_FEES
+                        Filter the orderbook based on maximum fees asked (naively, does not take into account the CoinJoin amount). Specify both absolute and relative fees comma separated,
+                        e.g., 1000,0.002 (default does not filter)
+  -v, --verbose         Increase logging verbosity to DEBUG
+```
+### Description
+This is a simple script to run simulations against a given JoinMarket orderbook.
+It emulates the JoinMarket order choosing algorithm to find out how often each maker offer gets selected by a taker.
+
+The only thing you need is an orderbook file in JSON format.
+To get that:
+* Run the [ob-watcher.py](https://github.com/JoinMarket-Org/joinmarket-clientserver/blob/master/docs/orderbook.md) script yourself and export it from there.
+* Find a public orderbook you trust and export it from there.
+* For testing, you can build the JSON file yourself, ideally a script should be made to make it trivial.
+
+### Requirements
+None.
+
+### Example
+```commandline
+python3 jmsim.py orderbook.json -f 1000,0.4 -a 120000
+11/11/2022 01:09:30 INFO: Filtering by CoinJoin amount
+11/11/2022 01:09:30 INFO: Filtered 49 offers
+11/11/2022 01:09:30 INFO: Filtering by max fee
+11/11/2022 01:09:30 INFO: Filtered 3 offers
+11/11/2022 01:09:30 INFO: Removing duplicates
+11/11/2022 01:09:30 INFO: Removed 0 offers
+11/11/2022 01:09:30 INFO: Running simulation for 10 trials (12700 CoinJoin each)...
+11/11/2022 01:09:35 INFO: Estimated picking chance (95% confidence):
+NICK (BOND VALUE)
+
+J53Aha9gj4w96Vqi (0.0000060668286756): 98.575% +/- 0.242%
+J58KVQjJf2PmTzH2 (0.0000052877962973): 97.959% +/- 0.291%
+J58rG3xDWFHTYKNY (0.0000046284301680): 97.130% +/- 0.194%
+J52E94aXXb45Camt (0.0000034399835598): 94.490% +/- 0.394%
+J5E8TeyyqboMf7ZV (0.0000020544660432): 85.795% +/- 0.780%
+J56SJcw5RqqoCunu (0.0000015965753131): 79.414% +/- 0.632%
+J5BYzMJBJcj7pJnC (0.0000015824203924): 79.160% +/- 0.808%
+J5UcuygR4cGkFnPO (0.0000011940295909): 70.487% +/- 0.945%
+J57P4drm3eDqQrP6 (0.0000009053333157): 60.719% +/- 0.941%
+J58GLDmebggitTug (0.0000008570142480): 59.058% +/- 0.389%
+J5DjhUAQDV5f6oky (0.0000008007055591): 56.783% +/- 0.860%
+J5DKuNtXsUYQuKQM (0.0000000079889032): 1.789% +/- 0.225%
+J58tmdiZSmCoZtWa (0.0000000043598671): 1.422% +/- 0.205%
+J52HViFEn3UEWU37 (0.0000000032181691): 1.361% +/- 0.203%
+J56x6miKTQrUHa3N (0.0000000031051122): 1.272% +/- 0.198%
+J54tHpQVyy1yGtTb (0.0000000023373594): 1.181% +/- 0.177%
+J57owrVxdx8P4RcP (0.0000000019626523): 1.161% +/- 0.266%
+J569vKm6fGYbBv8K (0.0000000012052017): 1.104% +/- 0.219%
+J5EAZtxNavsHF6aS (0.0000000009622733): 1.071% +/- 0.165%
+J5C4RjsXXRAQoDDP (0.0000000007775088): 1.024% +/- 0.136%
+J55eNmqmEcMw72wi (0.0000000001026433): 1.032% +/- 0.192%
+J58BYuKYDwSWThkC (0.0000000000798477): 1.033% +/- 0.146%
+J58FYTdN71rkWGZZ (0.0000000000444969): 1.046% +/- 0.142%
+J5B6AqVatJo57p4R (0.0000000000027347): 1.015% +/- 0.160%
+J57BzkF37C7MDiMn (0.0000000000009492): 1.022% +/- 0.141%
+J5A32Za2RHEYtCKk (0.0000000000000000): 1.029% +/- 0.152%
+J5DawQxjcQzUrVAU (0.0000000000000000): 1.033% +/- 0.179%
+J5C42iJ8gvvVUKqq (0.0000000000000000): 1.052% +/- 0.178%
+J5BjH1zdiRWnE45t (0.0000000000000000): 0.980% +/- 0.106%
+J5Ee8uoQVCsxHSj2 (0.0000000000000000): 1.029% +/- 0.178%
+J535PB6RKE3CF3Dz (0.0000000000000000): 1.012% +/- 0.220%
+[-SNIPPED-]
 ```
 ---
 
---
+++ jmsim.py
@@ -0,0 +1,258 @@
+#!/usr/bin/env python3
+"""
+Standalone script with no dependencies.
+The only thing you need is an orderbook.json file.
+To get that:
+    * Run the ob-watcher.py script yourself and export it.
+    * Find a public orderbook you trust and export it from there.
+    * For testing, you can build the JSON file yourself, ideally a script should be made to make it trivial.
+"""
+import sys
+from argparse import ArgumentParser, Namespace, ArgumentTypeError
+from decimal import Decimal
+from enum import Enum
+from json import loads
+from logging import Logger, getLogger, StreamHandler, Formatter
+from os.path import isfile
+from random import random, choices
+from statistics import stdev, mean
+from typing import List, Dict, Any, Tuple
+
+DESCRIPTION = """
+Given an orderbook file as JSON, e.g., exported from ob-watcher.py, run 
+multiple simulations to estimate the picking chances of each fidelity bond.
+"""
+
+# Same as JoinMarket default
+BONDLESS_ALLOWANCE = 0.125
+
+
+class ExitStatus(Enum):
+    """
+    Exit status codes.
+    """
+    SUCCESS = 0
+    FAILURE = 1
+    ARGERROR = 2
+
+
+def get_logger(verbose: bool) -> Logger:
+    logger = getLogger(__name__)
+    ch = StreamHandler()
+    if verbose:
+        # DEBUG
+        logger.setLevel(10)
+    else:
+        # INFO
+        logger.setLevel(20)
+    ch.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S'))
+    logger.addHandler(ch)
+    return logger
+
+
+def orderbook(path: str) -> Dict[str, Any]:
+    """
+    Open given filepath and return orderbook content.
+    """
+    if not isfile(path):
+        raise ArgumentTypeError(f"{path} is not a valid path")
+    try:
+        with open(path, "r", encoding="UTF-8") as ob:
+            return loads(ob.read())
+    except Exception as exc:
+        raise ArgumentTypeError('Not a valid orderbook JSON file') from exc
+
+
+def max_fees(fees: str) -> Tuple[int, float]:
+    abs_fee, rel_fee = fees.split(',')
+    try:
+        return int(abs_fee), float(rel_fee)
+    except Exception as exc:
+        raise ArgumentTypeError('Given max fees are not valid') from exc
+
+
+def get_args() -> Namespace:
+    parser = ArgumentParser(
+        usage="jmsim.py [options] orderbook",
+        description=DESCRIPTION,
+    )
+    parser.add_argument(
+        "orderbook",
+        action="store",
+        type=orderbook,
+        help="Path to the exported orderbook in JSON format",
+    )
+    parser.add_argument(
+        "-n",
+        "--maker-count",
+        action="store",
+        type=int,
+        dest="maker_count",
+        help="Number of counterparties to use in CoinJoin simulation, default 10",
+        default=10,
+    )
+    parser.add_argument(
+        "-t",
+        "--trials",
+        action="store",
+        type=int,
+        dest="trials",
+        help="Number of trials to estimate picking chances, default 10",
+        default=10,
+    )
+    parser.add_argument(
+        "-s",
+        "--sample_size",
+        action="store",
+        type=int,
+        dest="sample_size",
+        help="Number of CoinJoin simulations for each trial, default 100 times the number of (filtered) offers in the "
+             "orderbook",
+    )
+    parser.add_argument(
+        "-b",
+        "--bondless",
+        action="store",
+        type=float,
+        dest="bondless_allowance",
+        default=BONDLESS_ALLOWANCE,
+        help=f"Bondless allowance to use for CoinJoin simulation (instead of bondless_makers_allowance default, {BONDLESS_ALLOWANCE})",
+    )
+    parser.add_argument(
+        "-a",
+        "--amount",
+        action="store",
+        type=int,
+        dest="amount",
+        help="Filter orderbook based on CoinJoin amount in sats. If an offer can't satisfy this amount, "
+             "it's disregarded. Default does not filter",
+    )
+    parser.add_argument(
+        "-f",
+        "--max-fees",
+        action="store",
+        type=max_fees,
+        dest="max_fees",
+        help="Filter the orderbook based on maximum fees asked (naively, does not take into account the CoinJoin "
+             "amount). Specify both absolute and relative fees comma separated, e.g., 1000,0.002 (default does not "
+             "filter)",
+    )
+    parser.add_argument(
+        '-v',
+        '--verbose',
+        action='store_true',
+        help="Increase logging verbosity to DEBUG",
+    )
+    return parser.parse_args()
+
+
+def filter_ob_by_fees(offers: List[Dict[str, Any]], max_abs: int, max_rel: float) -> List[Dict[str, Any]]:
+    """
+    Return a new list of offers naively filtered by fees.
+    """
+    filtered_offers = []
+    for offer in offers:
+        cjfee = float(offer["cjfee"])
+        if "absoffer" in offer["ordertype"]:
+            if cjfee > max_abs:
+                continue
+        elif "reloffer" in offer["ordertype"]:
+            if cjfee > max_rel:
+                continue
+        else:
+            # It's an invalid order type
+            continue
+        filtered_offers.append(offer)
+    return filtered_offers
+
+
+def simulate_order_choose(weights: List[float],
+                          nicks: List[str],
+                          maker_count: int,
+                          bondless: float) -> list[str]:
+    """
+    Return list with the nicks of the selected offers.
+    The order choosing algos are intended to be exactly the same as the JoinMarket ones.
+    """
+    chosen_nicks = []
+    sum_weights = sum(weights)
+    # Makes copies to keep the original intact
+    sim_weights = weights[:]
+    sim_nicks = nicks[:]
+    for _ in range(maker_count):
+        if random() >= bondless and sum_weights != 0:
+            # Use fidelity_bond_weighted_order_choose
+            nick_index = choices(range(len(sim_nicks)), sim_weights, k=1)[0]
+        else:
+            # Use random_under_max_order_choose
+            nick_index = choices(range(len(sim_nicks)))[0]
+        chosen_nicks.append(sim_nicks[nick_index])
+        sum_weights -= sim_weights.pop(nick_index)
+        sim_nicks.pop(nick_index)
+
+    return chosen_nicks
+
+
+def simulate(weights: List[float], nicks: List[str], trials: int, sample_size: int, maker_count: int,
+             bondless: float) -> Dict[str, List[float]]:
+    """
+    Simulate `sample_size` CoinJoins for `trials` times.
+    Return a dict with key being the nick and value its corresponding results, one for trial.
+    Each result represents times_picked / sample_size.
+    """
+    res: Dict[str, List[float]] = {nick: [] for nick in nicks}
+    for _ in range(trials):
+        trial_res = {nick: 0 for nick in nicks}
+        for _ in range(sample_size):
+            chosen_nicks = simulate_order_choose(weights, nicks, maker_count, bondless)
+            for nick in chosen_nicks:
+                trial_res[nick] += 1
+        for nick in nicks:
+            res[nick].append(trial_res[nick] / sample_size)
+    return res
+
+
+def main() -> None:
+    args = get_args()
+    log = get_logger(args.verbose)
+    offers = args.orderbook["offers"]
+    if args.amount is not None:
+        log.info('Filtering by CoinJoin amount')
+        prev_length = len(offers)
+        offers = [offer for offer in args.orderbook["offers"] if offer["minsize"] <= args.amount <= offer["maxsize"]]
+        log.info(f'Filtered {prev_length - len(offers)} offers')
+    if args.max_fees is not None:
+        prev_length = len(offers)
+        log.info('Filtering by max fee')
+        offers = filter_ob_by_fees(offers, args.max_fees[0], args.max_fees[1])
+        log.info(f'Filtered {prev_length - len(offers)} offers')
+    # Remove duplicate counterparties, for now ignore fees
+    prev_length = len(offers)
+    log.info('Removing duplicates')
+    offers = {offer["counterparty"]: offer for offer in offers}
+    log.info(f'Removed {prev_length - len(offers)} offers')
+    offers_ = list(offers.values())
+    values = [order["fidelity_bond_value"] for order in offers_]
+    nicks = [order["counterparty"] for order in offers_]
+    # Run simulation to find picking chances of each bond in the orderbook
+    n_offers = len(offers)
+    if n_offers < args.maker_count:
+        log.error(f"Not enough offers in the orderbook ({n_offers}) "
+                  f"to perform simulation with {args.maker_count} counterparties")
+        sys.exit(ExitStatus.ARGERROR.value)
+    sample_size = n_offers * 100 if args.sample_size is None else args.sample_size
+    log.info(f"Running simulation for {args.trials} trials ({sample_size} CoinJoin each)...")
+    res = simulate(values, nicks, args.trials, sample_size, args.maker_count, args.bondless_allowance)
+    log.info("Estimated picking chance (95% confidence):")
+    sorted_orders = dict(sorted(offers.items(), key=lambda x: x[1]['fidelity_bond_value'], reverse=True))
+    print('NICK (BOND VALUE)\n')
+    for nick in sorted_orders:
+        mean_res = mean(res[nick])
+        stdev_res = stdev(res[nick]) * 1.96
+        bond_value = float(Decimal(offers[nick]["fidelity_bond_value"]) / Decimal(1e16))
+        print(
+            f'{offers[nick]["counterparty"]} ({bond_value:.16f}): {mean_res:.3%} +/- {stdev_res:.3%}')
+
+
+if __name__ == "__main__":
+    main()
